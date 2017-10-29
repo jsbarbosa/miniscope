dbf9408f6652cb542f06691ff0642261ad015132
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-10-29 12:51:00 +0000

    working

--- motor.c
+++ motor.c
@@ -2,40 +2,38 @@
 #include <stdint.h>                 // needed for uint8_t
 #include <util/delay.h>
 
+#define Mx0 _BV(PB0)
+#define Mx1 _BV(PB1)
+#define Mx2 _BV(PB2)
+#define Mx3 _BV(PB3)
 
+#define RIGHT 1
+#define LEFT 0
 
-uint16_t first(void)
-{
-	return 0x00000101;
-}
-
-uint16_t second(void)
-{
-	return 0x00001001;
-}
-
-uint16_t third(void)
-{
-	return 0x00001010;
-}
-
-uint16_t fourth(void)
-{
-	return 0x00000110;
-}
-
-void turn(uint16_t *PB, uint8_t direction, uint8_t delay_ms)
-{
-	delay_ms = 100;
-	if (direction == 1)
+void rotateX(uint8_t delay_ms, uint8_t direction)
+{	
+	delay_ms = 3;
+	
+	if(direction == RIGHT)
 	{
-		*PB = first();
+		PORTB = Mx0;
 		_delay_ms(delay_ms);
-		*PB = second();
+		PORTB = Mx1;
 		_delay_ms(delay_ms);
-		*PB = third();
+		PORTB = Mx2;
 		_delay_ms(delay_ms);
-		*PB = fourth();
+		PORTB = Mx3;
+		_delay_ms(delay_ms);
+	}
+	else
+	{
+		PORTB = Mx3;
+		_delay_ms(delay_ms);
+		PORTB = Mx2;
+		_delay_ms(delay_ms);
+		PORTB = Mx1;
+		_delay_ms(delay_ms);
+		PORTB = Mx0;
 		_delay_ms(delay_ms);
 	}
 }
@@ -47,7 +45,7 @@ int main(void)
 
     while(1 == 1)
     {
-		turn(&PORTB, 1, 1);
+		rotateX(10, LEFT);
     }
     return 0;
 }
