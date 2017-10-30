1e8eb938c490fea07bc81370628dc339efc623be
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-10-30 07:02:54 -0500

    win32

--- reader.py
+++ reader.py
@@ -1,19 +1,25 @@
 import serial
 from time import sleep
+from sys import platform
 
 BAUDRATE = 4800
 TIMEOUT = 1
 
 SLEEP_TIME = 0
 
-port = serial.Serial(port = '/dev/ttyUSB0', baudrate = BAUDRATE,
+if platform == "win32":
+	port = serial.Serial(port = 'COM3', baudrate = BAUDRATE,
+                     stopbits = serial.STOPBITS_ONE, parity = serial.PARITY_NONE,
+                        bytesize = serial.EIGHTBITS, timeout = TIMEOUT)
+else:
+	port = serial.Serial(port = '/dev/ttyUSB0', baudrate = BAUDRATE,
                      stopbits = serial.STOPBITS_ONE, parity = serial.PARITY_NONE,
                         bytesize = serial.EIGHTBITS, timeout = TIMEOUT)
 
 port.flush()
 
 while True:
-	port.write([0xB1])
+	port.write([0x01])
 	ans = int.from_bytes(port.read(), byteorder='big')
 	if ans == 0xff:
 		sleep(SLEEP_TIME)
