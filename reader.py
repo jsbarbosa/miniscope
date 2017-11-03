5fb440e95881572aec5bcfeddb35e3374c5a5229
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-11-02 21:28:41 -0500

    python

--- reader.py
+++ reader.py
@@ -1,29 +1,45 @@
 import serial
 from time import sleep
-from sys import platform
 
 BAUDRATE = 4800
 TIMEOUT = 1
 
-SLEEP_TIME = 0
+SLEEP_TIME = 1e-4
 
-if platform == "win32":
-	port = serial.Serial(port = 'COM3', baudrate = BAUDRATE,
-                     stopbits = serial.STOPBITS_ONE, parity = serial.PARITY_NONE,
-                        bytesize = serial.EIGHTBITS, timeout = TIMEOUT)
-else:
-	port = serial.Serial(port = '/dev/ttyUSB0', baudrate = BAUDRATE,
+XLEFT = 0xA0
+XRIGHT = 0xA1
+
+YLEFT = 0xB0
+YRIGHT = 0xB1
+
+ZLEFT = 0xC0
+ZRIGHT = 0xC1
+
+TURNSTEPS = 0
+
+port = serial.Serial(port = '/dev/ttyUSB0', baudrate = BAUDRATE,
                      stopbits = serial.STOPBITS_ONE, parity = serial.PARITY_NONE,
                         bytesize = serial.EIGHTBITS, timeout = TIMEOUT)
 
 port.flush()
 
+sequence = [ZLEFT]
+
+port.write([0xCA])
+ans = int.from_bytes(port.read(), byteorder='big')
+
+#for i in range(TURNSTEPS):
 while True:
-	port.write([0x01])
-	ans = int.from_bytes(port.read(), byteorder='big')
-	if ans == 0xff:
-		sleep(SLEEP_TIME)
-	port.write([0xA1])
-	ans = int.from_bytes(port.read(), byteorder='big')
-	if ans == 0xff:
-		sleep(SLEEP_TIME)
+    try:
+        for step in sequence:
+            port.write([step])
+            ans = int.from_bytes(port.read(), byteorder='big')
+            while ans != 0xff:
+                ans = int.from_bytes(port.read(), byteorder='big')
+                sleep(SLEEP_TIME)
+        TURNSTEPS += 1
+        print(TURNSTEPS)
+    except KeyboardInterrupt:
+        break
+
+print(TURNSTEPS)
